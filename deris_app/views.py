61662be8612eb76ea2650b9d5b47666789680247
Ana Geguchadze <anageguchadze22@gmail.com>
2025-02-16 17:05:08 +0100

    Add filters

--- deris_app/views.py
+++ deris_app/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from .models import Category, Animal
 from .serializers import CategorySerializer, AnimalSerializer
+from .filters import AnimalFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
@@ -15,6 +18,9 @@ class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
 class AnimalList(generics.ListCreateAPIView):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filter_class = AnimalFilter
+    search_fields = ['name', 'category__name', 'scientific_name', 'habitat', 'diet', 'status']
 
 
 class AnimalDetail(generics.RetrieveUpdateDestroyAPIView):
