4c81ccb89aae26ab0595782f83bd79a51a6e06a7
Ana Geguchadze <anageguchadze22@gmail.com>
2025-02-19 21:16:21 +0100

    Add model Pet

--- deris_app/models.py
+++ deris_app/models.py
@@ -18,3 +18,16 @@ class Animal(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=100, blank=True, null=True)
+    is_available = models.BooleanField(default=True)  
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
