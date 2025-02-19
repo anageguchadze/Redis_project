42f1c8450c84edaa79386a9979b51f5e01cfdd80
Ana Geguchadze <anageguchadze22@gmail.com>
2025-02-19 21:19:40 +0100

    Add admin for Category, Animal, Pet

--- deris_app/admin.py
+++ deris_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Animal, Pet
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Animal)
+admin.site.register(Pet)
