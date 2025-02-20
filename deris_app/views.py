9a89c3f5624b5873792d7b10b746520e37ae5d54
Ana Geguchadze <anageguchadze22@gmail.com>
2025-02-20 19:49:10 +0100

    Add function for caching Animallist

--- deris_app/views.py
+++ deris_app/views.py
@@ -4,6 +4,9 @@ from .serializers import CategorySerializer, AnimalSerializer, PetSerializer
 from .filters import AnimalFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from django.core.cache import cache
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
@@ -18,10 +21,18 @@ class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
 class AnimalList(generics.ListCreateAPIView):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filter_class = AnimalFilter
-    search_fields = ['name', 'category__name', 'scientific_name', 'habitat', 'diet', 'status']
-
+    # filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    # filter_class = AnimalFilter
+    # search_fields = ['name', 'category__name', 'scientific_name', 'habitat', 'diet', 'status']
+    def list(self, request, *args, **kwargs):
+        cache_key = 'animal_list'
+        cache_time = 60 * 5
+        data = cache.get(cache_key)
+        if not data:
+            animals = Animal.objects.all()
+            data = AnimalSerializer(animals, many=True).data
+            cache.set(cache_key, data, cache_time)
+        return Response(data)
 
 class AnimalDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Animal.objects.all()
